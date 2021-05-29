be9e6dbf041e0b5870b724796f4319b464841067
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-05-29 19:35:02 -0400

    display today's weather good to go

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ var mainInfo = $(".maininfo")
 var forecastList = $(".forecast")
 var submitButton = $(".btnleft")
 var api = "6e4ceb0e004fbf2de1aefec5c659a282"
+var uv = $(".uv")
+var rightHide = $(".asideright")
 
 
 
@@ -27,34 +29,41 @@ function getDataAPI(location) {
           
         });
 }
+
+//Display weather info for the current day
 function todayInfo(data) {
-    var temp = data.main.temp
-    var wind = data.wind.speed
-    var hum = data.main.humidity
-    var lon = data.coord.lon
-    var lat = data.coord.lat
-    var icon = data.weather.icon
-    console.log(data.main.temp);
-    var urluv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + api + "&unit=metric"
+    rightHide.removeClass("hide")
+    var urluv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude={part}&appid=" + api + "&unit=metric"
     fetch(urluv)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.current.uvi);
-      
+      var uv = data.current.uvi
+      mainInfo.children().eq(4).children().eq(1).text(uv)
+      if (0<=uv<=3) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: green");
+      } else if (3<=uv<=6) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: yellow");
+      } else if (6<=uv<=7) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: orange");
+      } else if (7<=uv<=10) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: red");
+      }
     });
-    mainInfo.children().eq(1).text("Temperature" + temp + "C")
-
-
-
-
+    mainInfo.children().eq(0).children().eq(0).text(data.name)
+    mainInfo.children().eq(0).children().eq(1).text(moment().format('L')  )
+    mainInfo.children().eq(0).children().eq(2).attr("src", "http://openweathermap.org/img/w/"  + data.weather[0].icon + ".png")
+    mainInfo.children().eq(1).text("Temperature: " + data.main.temp + " C")
+    mainInfo.children().eq(2).text("Wind: " + data.wind.speed + " m/s")
+    mainInfo.children().eq(3).text("Humidity: " + data.main.humidity + "%")
+    mainInfo.children().eq(4).children().eq(0).text("UV: ")
 }
 
 
-// function foreCast(location) {
-
-// }
+ function foreCast(location) {
+    
+ }
 
 
 // function storeLast(location) {
