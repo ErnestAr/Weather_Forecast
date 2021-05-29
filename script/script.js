abe896bbd001808436c61dc081093c7b9ca813b4
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-05-29 14:14:55 -0400

    basic style layout

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var locationList = $(".locationlist")
+var mainInfo = $(".maininfo")
+var forecastList = $(".forecast")
+var submitButton = $(".btnleft")
+
+
+
+
+function showLocation(location) {
+
+}
+
+function mainInfo(location) {
+
+}
+
+
+function foreCast(location) {
+
+}
+
+function
+
+
+submitButton.on("click", function() {
+    showDate()
+    mainDate()
+    showLocation()
+})
