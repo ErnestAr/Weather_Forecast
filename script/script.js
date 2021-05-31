ec002e3de1580e6e771f8ce5e347d20fd8abd1b4
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-05-30 20:53:37 -0400

    forecast and today's weather display working well, todo: load local storage

--- script/script.js
+++ script/script.js
@@ -2,9 +2,14 @@ var locationList = $(".locationlist")
 var mainInfo = $(".maininfo")
 var forecastList = $(".forecast")
 var submitButton = $(".btnleft")
-var api = "6e4ceb0e004fbf2de1aefec5c659a282"
 var uv = $(".uv")
 var rightHide = $(".asideright")
+var foreHd = $(".forehd")
+
+var api = "6e4ceb0e004fbf2de1aefec5c659a282"
+
+var count=0;
+
 
 
 
@@ -13,10 +18,31 @@ var rightHide = $(".asideright")
 // function displayLS() {
 
 // }
+// Add location to the list if it's not in the list, add location to local storage
+function saveLocation(location) {
+          hasloc = false;
+          var loclist = locationList.children()
+          console.log(loclist);
+          for (let i = 0; i < loclist.length; i++) {
+            var classLoc = loclist[i].getAttribute("class")
+              if(classLoc === location) {
+                hasloc = true
+              }
+          }
+          if (!hasloc) {
+            var newLi = $("<li>")
+            newLi.text(location)
+            newLi.attr("class", location)
+            locationList.append(newLi)
+            hasLoc = false;
+            localStorage.setItem( count, location)
+            count++
+          }
+      }
 
-// function saveLocation(location) {
 
-// }
+
+//initiate data fetching and display obtained data 
 function getDataAPI(location) {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + api + "&units=metric"
     fetch(url)
@@ -24,16 +50,16 @@ function getDataAPI(location) {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
+          // console.log(data);
           todayInfo(data);
-          
+          foreCast(data);
         });
 }
 
-//Display weather info for the current day
+//Display weather info for the current day, show uv danger level with coloring 
 function todayInfo(data) {
     rightHide.removeClass("hide")
-    var urluv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude={part}&appid=" + api + "&unit=metric"
+    var urluv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude={part}&appid=" + api + "&units=metric"
     fetch(urluv)
     .then(function (response) {
       return response.json();
@@ -41,14 +67,14 @@ function todayInfo(data) {
     .then(function (data) {
       var uv = data.current.uvi
       mainInfo.children().eq(4).children().eq(1).text(uv)
-      if (0<=uv<=3) {
-        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: green");
-      } else if (3<=uv<=6) {
-        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: yellow");
-      } else if (6<=uv<=7) {
-        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: orange");
-      } else if (7<=uv<=10) {
-        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: red");
+      if (0 <=  uv && uv < 3) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: green; border-radius: 3px;");
+      } else if (3 <= uv && uv <6) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: yellow; border-radius: 3px;");
+      } else if (6 <= uv && uv < 7) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: orange; border-radius: 3px;");
+      } else if (7 <= uv ) {
+        mainInfo.children().eq(4).children().eq(1).attr("style", "background-color: red; border-radius: 3px;");
       }
     });
     mainInfo.children().eq(0).children().eq(0).text(data.name)
@@ -58,34 +84,47 @@ function todayInfo(data) {
     mainInfo.children().eq(2).text("Wind: " + data.wind.speed + " m/s")
     mainInfo.children().eq(3).text("Humidity: " + data.main.humidity + "%")
     mainInfo.children().eq(4).children().eq(0).text("UV: ")
+    
 }
 
+//Display forecast for 5 days
+function foreCast(data) {
+    forecastList.removeClass("hide")
+    foreHd.removeClass("hide")
+    var urluv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude={part}&appid=" + api + "&units=metric"
+    fetch(urluv)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var forecastData = data.daily
+      for (let i = 0; i < 5; i++) {
+        forecastList.children().eq(i).children().eq(0).text(moment.unix(forecastData[i].dt).format("MM/DD/YYYY"))
+        forecastList.children().eq(i).children().eq(1).attr("src", "http://openweathermap.org/img/w/"  + forecastData[i].weather[0].icon + ".png")
+        forecastList.children().eq(i).children().eq(2).text("Temp: " + forecastData[i].temp.day + " C")
+        forecastList.children().eq(i).children().eq(3).text("Wind: " + forecastData[i].wind_speed + " m/s")
+        forecastList.children().eq(i).children().eq(4).text("Humidity: " + forecastData[i].humidity + "%")
+      }
+    })
+  }
 
- function foreCast(location) {
-    
- }
 
 
-// function storeLast(location) {
-
-// }
-
+//Call data by clicking submit button
 submitButton.on("click",  function(event) {
     event.preventDefault()
     var location = submitButton.parent().children().eq(0).val()
     getDataAPI(location)   
     // foreCast(location)
-    // saveLocation(location)
+    saveLocation(location)
 })
 
-
-
-// locationList.on("click", function() {
-//     var location = ...
-//     mainInfo(location)
-//     foreCast(location)
-//     saveLocation(location)
-// })
-
+//Call data by clicking location in the list
+ locationList.on("click", function(event) {
+    var liItem = event.target
+    console.log(liItem);
+    var location = liItem.getAttribute("class")
+    getDataAPI(location)
+ })
 
 
